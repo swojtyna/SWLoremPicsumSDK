d1e7dea2edc424b02f297c6bb0c4e15c46a204fd
Sebastian Wojtyna <wojtyna.sebastian@gmail.com>
2018-04-02 12:26:11 +0200

    Add base classes

---
+++ SWLoremPicsumSDK/SWLoremPicsumSDK/Source/Features/PhotoWebService.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoWebService.swift
+//  SWLoremPicsumSDK
+//
+//  Created by Sebastian Wojtyna on 02.04.2018.
+//  Copyright © 2018 Sebastian Wojtyna. All rights reserved.
+//
+
+import Foundation
+
+public class PhotoWebService {
+
+    public func photo() -> String {
+        return "👏 To be implemented "
+    }
+
+}
---
+++ SWLoremPicsumSDK/SWLoremPicsumSDK/Source/Features/PhotosListWebService.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosListWebService.swift
+//  SWLoremPicsumSDK
+//
+//  Created by Sebastian Wojtyna on 02.04.2018.
+//  Copyright © 2018 Sebastian Wojtyna. All rights reserved.
+//
+
+import Foundation
+
+public class PhotosListWebService {
+
+    public func photo() -> [String] {
+        return ["💀 To be implemented ",
+                "🎉 To be implemented ",
+                "🐥 To be implemented "]
+    }
+
+}
