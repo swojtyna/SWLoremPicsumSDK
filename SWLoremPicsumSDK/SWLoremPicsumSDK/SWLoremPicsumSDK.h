392d3d97723b647e82582f8d88f14401ed99bf07
Sebastian Wojtyna <wojtyna.sebastian@gmail.com>
2018-04-02 11:50:06 +0200

    Inital commit

---
+++ SWLoremPicsumSDK/SWLoremPicsumSDK/SWLoremPicsumSDK.h
@@ -0,0 +1,19 @@
+//
+//  SWLoremPicsumSDK.h
+//  SWLoremPicsumSDK
+//
+//  Created by Sebastian Wojtyna on 02.04.2018.
+//  Copyright © 2018 Sebastian Wojtyna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWLoremPicsumSDK.
+FOUNDATION_EXPORT double SWLoremPicsumSDKVersionNumber;
+
+//! Project version string for SWLoremPicsumSDK.
+FOUNDATION_EXPORT const unsigned char SWLoremPicsumSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWLoremPicsumSDK/PublicHeader.h>
+
+
