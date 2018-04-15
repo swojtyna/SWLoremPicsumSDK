ef910889fb988d9d2a18971a2efd01fd53052572
Sebastian Wojtyna <wojtyna.sebastian@gmail.com>
2018-04-15 22:25:50 +0200

    Add OHHTTPStubs/Swift pod

--- SWLoremPicsumSDK/Pods/Target Support Files/Pods-SWLoremPicsumSDKTests/Pods-SWLoremPicsumSDKTests-frameworks.sh
+++ SWLoremPicsumSDK/Pods/Target Support Files/Pods-SWLoremPicsumSDKTests/Pods-SWLoremPicsumSDKTests-frameworks.sh
@@ -137,11 +137,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Moya/Moya.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/OHHTTPStubs/OHHTTPStubs.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Moya/Moya.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/OHHTTPStubs/OHHTTPStubs.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
